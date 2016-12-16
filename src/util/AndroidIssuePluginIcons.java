f66fd5325f25ba50180d17d90d3f0db1b57a8745
Matthew Miller <mathieu7@gmail.com>
2016-12-16 14:45:33 -0500

    Add custom icon for toolbar window

---
+++ src/util/AndroidIssuePluginIcons.java
@@ -0,0 +1,10 @@
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Created by Matt on 12/16/2016.
+ */
+public interface AndroidIssuePluginIcons {
+    Icon STRUCTURE_TOOL_WINDOW = IconLoader.getIcon("/toolWindowStructure.png");
+}
