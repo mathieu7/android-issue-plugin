6b4a915bcfba7d48e3084c2826536a1b035901f9
Matthew Miller <mathieu7@gmail.com>
2017-02-22 16:36:56 -0500

    Refactor DownloadTask to use localisation

--- src/tasks/DownloadTask.java
+++ src/tasks/DownloadTask.java
@@ -8,6 +8,7 @@ import model.IssuePost;
 import model.IssueComment;
 import org.jetbrains.annotations.NotNull;
 import scraper.AndroidIssueScraper;
+import util.PluginTextUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,23 +20,17 @@ import java.util.concurrent.TimeUnit;
  * Task to download issues from Android Issue Tracker
  */
 public final class DownloadTask extends Task.Backgroundable {
-
+    private static final long EXECUTOR_TIMEOUT = 240L;
 
     public interface Listener {
         void onDownloadCompleted();
         void onDownloadFailed(Exception exception);
     }
 
-    /**
-     * Progress Indicator strings (TODO: Replace with localized strings)
-     */
-    private static final String PROGRESS_INDICATOR_TITLE = "Downloading Android Issues...";
-    private static final String DOWNLOAD_FAILED_STRING = "Could not download issues from https://code.google.com/android";
-
     private Listener mListener;
 
     public DownloadTask(final Project project, final Listener listener) {
-        super(project, PROGRESS_INDICATOR_TITLE, false);
+        super(project, PluginTextUtil.getString("download_task_indicator_title"), false);
         setListener(listener);
     }
 
@@ -45,60 +40,56 @@ public final class DownloadTask extends Task.Backgroundable {
 
     @Override
     public void run(@NotNull ProgressIndicator progressIndicator) {
-        progressIndicator.setText(PROGRESS_INDICATOR_TITLE);
+        progressIndicator.setText(PluginTextUtil.getString("download_task_indicator_title"));
         progressIndicator.setIndeterminate(false);
+        ArrayList<IssuePost> issues = new ArrayList<>();
         try {
             // First, download all the issue posts
-            ArrayList<IssuePost> issues = (ArrayList<IssuePost>)
+            issues = (ArrayList<IssuePost>)
                     AndroidIssueScraper.getInstance(getProject()).getIssues(progressIndicator);
 
             // Second, write all the posts to storage
             AndroidIssueManager.writePostsToStorage(issues);
-
-            ExecutorService executorService = Executors.newFixedThreadPool(5);
-            // Then, For each post, download the issue thread associated by id
-            // and write to storage.
-            int count = 0;
-            for (IssuePost issue: issues) {
-                final int c = count;
-                executorService.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println(c + " - Writing issue thread to storage: "
-                                + issue.getId());
-                        try {
-                            List<IssueComment> issueComments =
-                                    AndroidIssueScraper.getInstance(getProject()).getIssueDetail(issue);
-                            AndroidIssueManager.writeThreadToStorage(
-                                    issue.getId(), issueComments);
-                        } catch (AndroidIssueScraper.IssueScraperException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                });
-                count++;
-            }
-
-            try {
-                executorService.awaitTermination(240L, TimeUnit.SECONDS);
-            } catch (InterruptedException exception) {
-                exception.printStackTrace();
-                System.out.println("Timeout reached, executorService suspended");
-            }
-
-
-            // notify observer that we've finished.
-            if (mListener != null) {
-                mListener.onDownloadCompleted();
-            }
-
         } catch (AndroidIssueScraper.IssueScraperException ex) {
             ex.printStackTrace();
-            progressIndicator.setText(DOWNLOAD_FAILED_STRING);
+            progressIndicator.setText(PluginTextUtil.getString("download_task_failed"));
             progressIndicator.cancel();
             if (mListener != null) {
                 mListener.onDownloadFailed(ex);
             }
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        // Then, For each post, download the issue thread associated by id
+        // and write to storage.
+        for (IssuePost issue : issues) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Writing issue thread to storage: "
+                            + issue.getId());
+                    try {
+                        List<IssueComment> issueComments =
+                                AndroidIssueScraper.getInstance(getProject()).getIssueDetail(issue);
+                        AndroidIssueManager.writeThreadToStorage(
+                                issue.getId(), issueComments);
+                    } catch (AndroidIssueScraper.IssueScraperException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        try {
+            executorService.awaitTermination(EXECUTOR_TIMEOUT,
+                    TimeUnit.SECONDS);
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+            System.out.println("Timeout reached, executorService suspended");
+        }
+        // notify observer that we've finished.
+        if (mListener != null) {
+            mListener.onDownloadCompleted();
         }
     }
 }
